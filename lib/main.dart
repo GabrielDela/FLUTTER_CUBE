4bc0729ceee6925441a804e99ad9e160fe221364
Amandine <amandine.dislaire@gmail.com>
2022-02-18 21:21:06 +0100

    projet de base

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:cube/Auth.dart';
-import 'package:cube/Colors.dart';
+import 'package:cube/pages/page_auth.dart';
 import 'package:flutter/material.dart';
-
 
 void main() {
   runApp(const MyApp());
