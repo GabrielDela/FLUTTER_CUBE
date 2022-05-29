67534458b66af07a4f3b95c53799a424d4bcb045
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-05-29 19:51:22 +0200

    partage ressource

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       title: 'CUBE',
       // home: PageHome(),
-      home: PageAuth(),
+      home: PageProfilAmi(),
       debugShowCheckedModeBanner: false,
     );
   }
